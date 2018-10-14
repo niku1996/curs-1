88f50735b4c969e2d1d60b62630ef6e5a5222196
krypton17 <maxdodon25@gmail.com>
2018-10-14 21:03:45 +0300

    Added Owner

---
+++ house/house/house/Dog.java
@@ -0,0 +1,22 @@
+package house;
+
+public class Dog {
+    public static double weight;   //g
+    public static String name;     //name of house.Dog
+    public static int age;         //years
+    public Dog(String name, double weight, int age){
+        this.weight = weight;
+        this.name = name;
+        this.age = age;
+    }
+    public static void info(){
+        System.out.println("Name " + name + "\n Weight " + weight + " grams" + "\n Age " + age + " years");
+    }
+
+    public static void eat(int qauntity){
+        weight += qauntity * 0.1;
+        age++;
+    }
+}
+
+
---
+++ house/house/main/main.java
@@ -0,0 +1,12 @@
+package main;
+
+import house.Dog;
+
+public class main {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Snow",520,3);
+        dog1.info();
+        dog1.eat(120);
+        dog1.info();
+    }
+}
