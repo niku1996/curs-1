fd9f9f96c1b69c34e59706ebabce1f945a8b4d8b
krypton17 <maxdodon25@gmail.com>
2018-10-17 19:53:51 +0300

    some changes

--- CodingApp/untitled/src/main/main.java
+++ CodingApp/untitled/src/main/main.java
@@ -5,6 +5,9 @@ public class main {
     public static void main(String[] args) {
         airplane plane1 = new airplane(1000,100);
         plane1.setFuel(300);
+//        while(plane1.getFuel() != 0){
+//
+//        }
         System.out.println(plane1); // .toString())
 
     }
