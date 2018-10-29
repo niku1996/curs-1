275528cc9be4bac4d2a95a37295831692e6d6d77
krypton17 <maxdodon25@gmail.com>
2018-10-29 17:06:38 +0200

    Finished the logic of app

--- FakeProduct/src/entities/Product.java
+++ FakeProduct/src/entities/Product.java
@@ -1,16 +1,15 @@
 package entities;
-
 import com.github.javafaker.Faker;
-
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Product {
-    public static String product;
     String category;
     String name;
     String manufacture;
@@ -27,38 +26,87 @@ public class Product {
         this.expires = f.date().future(100,TimeUnit.DAYS);
     }
 
-    public Product(String category, String name, float price, String manufacture, Date date){
+    public Product(String category, String name, float price, String manufacture){
             this.category = category;
             this.name = name;
             this.price = price;
             this.manufacture = manufacture;
-
     }
-    public static void createProduct(){
-        Product p = new Product();
+    public static void Menu(){
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        String product;
+        product = sc.next();
+        if (product.equals("Create")){
+            System.out.println("Created new Product!!!");
+            Product p = new Product();
+            count++;
+            product = sc.next();
+            if (product.equals("Save")){
+                p.saveProduct();
+                System.out.println("Saved!!!");
+                product = sc.next();
+            }else {
+                System.out.println("Didn't saved");
+            }
+            if (product.matches("Load")){
+                p.loadProduct("/home/krypt0n/IdeaProjects/FakeProduct/src/savedProduct.txt");
+                System.out.println("Loaded!!!");
+            }else {
+                System.out.println("Didn't loaded");
+            }
+        }if (product.equals("Createaproduct") && count == 1 ){
+            System.out.println("You already have a product");
+        }else {
+            System.out.println("You didn't typed anything or typed incorrect");
+        }
     }
-    public static void saveProduct(String file){
+    public static void printMenu(){
+        System.out.println("###############" +
+                "           \nMENU" +
+                "           \n###############" +
+                "           \nPlease,select what you want to do;" +
+                "           \nCreate a product;" +
+                "           \nSave to a file;" +
+                "           \nLoad from a file;" +
+                "           \nOr exit" +
+                "           \n###############" +
+                "           \n\n");
+    }
+    private void saveProduct(){
         try {
-            FileWriter fw = new FileWriter(new File(file));
-            fw.write();
+            FileWriter fw = new FileWriter(new File("/home/krypt0n/IdeaProjects/FakeProduct/src/savedProduct.txt"));
+            fw.write("Category: " + category + "Name: " + name + "Price: " + price + "Manufacturer: " + manufacture + "Expires: " + expires);
+            System.out.println("Created: " + fw);
             fw.close();
         }catch (IOException e){
             System.err.println("File Error\n");
             e.printStackTrace();
         }
     }
+    private void loadProduct(String file){
+        try {
+            FileReader fr = new FileReader(new File(file));
+            Scanner sr = new Scanner(new File("/home/krypt0n/IdeaProjects/FakeProduct/src/savedProduct.txt"));
+            String data = sr.nextLine();
+            System.out.println("Loaded!!!\n" + data);
+            fr.close();
+        }catch (IOException e){
+            System.err.println("File Error");
+            e.printStackTrace();
+        }
+    }
     public String toString(){
         return "Product category: " + category + " Name: " + name + "\n Price: " + price + "\n Manufacturer: " + manufacture + "\n Date: " + expires;
     }
-    public void saveToFile(String file_name){
-        try{
-            FileWriter fw = new FileWriter(new File(file_name));
-            fw.write(this.toString());
-            fw.close();
-        }catch (IOException e){
-            e.printStackTrace();
-            System.err.println("File Error");
-        }
-
-    }
+//    public void saveToFile(String file_name){
+//        try{
+//            FileWriter fw = new FileWriter(new File(file_name));
+//            fw.write(this.toString());
+//            fw.close();
+//        }catch (IOException e){
+//            e.printStackTrace();
+//            System.err.println("File Error");
+//        }
+//    }
 }
--- FakeProduct/src/main/Application.java
+++ FakeProduct/src/main/Application.java
@@ -1,44 +1,13 @@
 package main;
-
 import entities.Product;
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Product.product = sc.next();
 
         while(true){
-            System.out.println("###############\n" +
-                    "           MENU\n" +
-                    "           ###############" +
-                    "           Please,select what you want to do;" +
-                    "           Create a product;" +
-                    "           Save to a file;" +
-                    "           Load from a file;" +
-                    "           Or exit");
-
-            switch (Product.product){
-                case "Create a product" : Product.createProduct();
-                case "Save to a file"   : Product.saveProduct("/home/krypt0n/IdeaProjects/FakeProduct/src/newProduct.txt");
-                case "Load from a file" : 
-            }
-
+            Product.printMenu();
+            Product.Menu();
+            break;
         }
-
-        // while(){
-        //Menu
-            // 1. create product
-            // 2.save file
-            // 3.load
-            // 4.exit
-       //}
-
-//        Product p = new Product("Drinks",
-//                                    "Coca Cola",
-//                                    15.50f,
-//                              "Coca inc.",
-//                                           new Date()
-//        );
     }
 }
